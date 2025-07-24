6adfa7417558ece8476e26432a79724bb2a5df52
Bhoomika <bhoomika0khatri@gmail.com>
2025-07-24 15:19:07 -0400

    shell

---
+++ folder.sh
@@ -0,0 +1,3 @@
+export DS9DATA_ROOTDIR='/Users/faerest/Desktop/file_set3'
+export DS9SRC_ROOTDIR='/Users/faerest/Desktop/pantheon_test_file/test1'
+export DS9OUT_ROOTDIR=$DS9DATA_ROOTDIR/regionfiles
