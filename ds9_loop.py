6800d88617eea881c57049d8b1c0ace518864d77
FaeR7 <erest2@jh.edu>
2025-07-30 11:53:27 -0400

    fixed PA, major axis arcsec, fits images in pantheon_data_folder

--- ds9_loop.py
+++ ds9_loop.py
@@ -38,6 +38,13 @@ def define_args():
     )
 
     parser.add_argument(
+        "--unmatched_pantheon_path", 
+        type=str, 
+        default=f'{tabledir}/data_collection_files/data_files/unmatched_pantheon.csv', 
+        help="Path to the list of supernovas CSV file (default=%(default)s)"
+    )
+
+    parser.add_argument(
         "--brightest_galaxy_path", 
         type=str, 
         default=f'{tabledir}/data_collection_files/data_files/brightest_galaxy.csv', 
@@ -67,32 +74,40 @@ def define_args():
     return parser.parse_args()
 
 
+
 # ds9:
-def make_region_command_for_position(ra,dec,position_angle,major_axis,minor_axis,name=None,symbol='circle',color='red',width=4,size='10"'):
-    print(f'\nMaking region command: RA_Galaxy {ra}, Dec_Galaxy {dec}, name {name}, Position_Angle {position_angle}, Major_Axis {major_axis}, Minor_Axis {minor_axis}')
+def make_ellipse_command_for_position(ra,dec,position_angle,major_axis,minor_axis,name=None,color='red',width=4):
+    print(f'\nMaking ellipse command: RA_Galaxy {ra}, Dec_Galaxy {dec}, name {name}, Position_Angle {position_angle}, Major_Axis {major_axis}, Minor_Axis {minor_axis}')
     name='{'+f'{name}'+'}'
     s = f'ellipse({ra},{dec},{major_axis}",{minor_axis}",{position_angle}) # color={color} width={width} text={name}'
     print(s)
     return(s)
 
-def get_region_info(cd_matrix, cov_matrix):
-    # 3. World-space covariance matrix (deg²)
-    cov_world = cd_matrix @ cov_matrix @ cd_matrix.T
+def make_circle_command_for_position(ra,dec,radius_arcsec=1,name=None,color='blue',width=5):
+    print(f'\nMaking circle command: RA {ra}, Dec {dec}, name {name},')
+    name='{'+f'{name}'+'}'
+    s = f'circle({ra},{dec},{radius_arcsec}") # color={color} width={width} text={name}'
+    print(s)
+    return(s)
 
-    # 4. Eigen-decomposition
-    eigvals, eigvecs = np.linalg.eigh(cov_world)  # always returns sorted values
+# def get_region_info(cd_matrix, cov_matrix):
+#     # 3. World-space covariance matrix (deg²)
+#     cov_world = cd_matrix @ cov_matrix @ cd_matrix.T
+
+#     # 4. Eigen-decomposition
+#     eigvals, eigvecs = np.linalg.eigh(cov_world)  # always returns sorted values
     
-    a_arcsec = 3600 * np.sqrt(eigvals[1])  # major axis
-    b_arcsec = 3600 * np.sqrt(eigvals[0])  # minor axis
+#     a_arcsec = 3600 * np.sqrt(eigvals[1])  # major axis
+#     b_arcsec = 3600 * np.sqrt(eigvals[0])  # minor axis
 
-    # 5. Position angle (in degrees, from North to East)
-    vec = eigvecs[:, 1]  # eigenvector of major axis
-    theta_rad = np.arctan2(vec[0], vec[1])  # careful: note the order (RA, Dec)
-    theta_deg = np.degrees(theta_rad)
+#     # 5. Position angle (in degrees, from North to East)
+#     vec = eigvecs[:, 1]  # eigenvector of major axis
+#     theta_rad = np.arctan2(vec[0], vec[1])  # careful: note the order (RA, Dec)
+#     theta_deg = np.degrees(theta_rad)
 
-    theta_deg = (theta_deg + 360) % 180
+#     theta_deg = (theta_deg + 360) % 180
 
-    return theta_deg, a_arcsec, b_arcsec
+#     return theta_deg, a_arcsec, b_arcsec
 
 
 def get_region_info2(table):
@@ -105,41 +120,42 @@ def get_region_info2(table):
         table.loc[ix,'a_pixels'] = a
         table.loc[ix,'b_pixels'] = b
 
-        theta_rad = 0.5 * math.atan2(2 * cxy, cxx - cyy)
+        #theta_rad = 0.5 * math.atan2(2 * cxy, cxx - cyy)
         theta_deg = math.degrees(theta_rad)
         table.loc[ix,'theta_deg'] = theta_deg
+        table.loc[ix,'PA_deg'] = theta_deg+90.0
         table.loc[ix,'theta_rad'] = theta_rad
 
         a_arcsec = math.fabs(row['CD1_1'])*3600.0*a
         b_arcsec = math.fabs(row['CD1_1'])*3600.0*b
-        table.loc[ix,'a_arcsec'] = a
-        table.loc[ix,'b_arcsec'] = b
+        table.loc[ix,'a_arcsec'] = a_arcsec
+        table.loc[ix,'b_arcsec'] = b_arcsec
 
         continue
 
-        trace = cxx + cyy
-        delta = (cxx - cyy) / 2
-        root_term = math.sqrt(delta**2 + cxy**2)
-        lambda1 = (trace / 2) + root_term
-        lambda2 = (trace / 2) - root_term
+        # trace = cxx + cyy
+        # delta = (cxx - cyy) / 2
+        # root_term = math.sqrt(delta**2 + cxy**2)
+        # lambda1 = (trace / 2) + root_term
+        # lambda2 = (trace / 2) - root_term
 
      
-        a = math.sqrt(abs(max(lambda1, lambda2)))
-        b = math.sqrt(abs(min(lambda1, lambda2)))
-        table.loc[ix,'a_pixels'] = a
-        table.loc[ix,'b_pixels'] = b
+        # a = math.sqrt(abs(max(lambda1, lambda2)))
+        # b = math.sqrt(abs(min(lambda1, lambda2)))
+        # table.loc[ix,'a_pixels'] = a
+        # table.loc[ix,'b_pixels'] = b
 
-        theta_rad = 0.5 * math.atan2(2 * cxy, cxx - cyy)
-        theta_deg = math.degrees(theta_rad)
-        table.loc[ix,'theta_deg'] = theta_deg
-        table.loc[ix,'theta_rad'] = theta_rad
+        # theta_rad = 0.5 * math.atan2(2 * cxy, cxx - cyy)
+        # theta_deg = math.degrees(theta_rad)
+        # table.loc[ix,'theta_deg'] = theta_deg
+        # table.loc[ix,'theta_rad'] = theta_rad
 
-        a_arcsec = math.fabs(row['CD1_1'])*a
-        b_arcsec = math.fabs(row['CD1_1'])*b
-        table.loc[ix,'a_arcsec'] = a
-        table.loc[ix,'b_arcsec'] = b
+        # a_arcsec = math.fabs(row['CD1_1'])*a
+        # b_arcsec = math.fabs(row['CD1_1'])*b
+        # table.loc[ix,'a_arcsec'] = a
+        # table.loc[ix,'b_arcsec'] = b
 
-        print(f'coordinates in degrees and x and y length:{theta_deg}, {a}, {b}')
+        print(f'PA(deg) and a, b of ellipse:{PA_deg}, {a}, {b}')
     return(table)
 
 
@@ -150,6 +166,7 @@ def save_region_file(filepath, output):
             file.write(str(line) + "\n")
     file.close()
 
+#combining fits_summary and brightest_galaxy
 def combine_fs_and_bg(supernova_names, fits_summary, brightest_galaxy):
     for sn_name in supernova_names:
         print("SN name: ",sn_name)
@@ -157,7 +174,10 @@ def combine_fs_and_bg(supernova_names, fits_summary, brightest_galaxy):
         sn_ix_bg = brightest_galaxy[brightest_galaxy["SNID"] == sn_name].index.values
         print("Brightest galaxy indices: ",sn_ix_bg)
         if len(sn_ix_bg) == 0:
-            raise RuntimeError(f'could not find indices for galaxy {sn_name}')
+            ######
+            print(f'WARNING! could not find indices for galaxy {sn_name}, skipping')
+            continue
+#            raise RuntimeError(f'could not find indices for galaxy {sn_name}')
 
         sn_ix = np.where(fits_summary["SNID"] == sn_name)[0]
         print("Fits summary indices: ",sn_ix)
@@ -165,7 +185,7 @@ def combine_fs_and_bg(supernova_names, fits_summary, brightest_galaxy):
         for index in sn_ix_bg:
             filename = brightest_galaxy.loc[index,'File_key']
             filename_ix_fs = sn_ix[np.where(fits_summary.loc[sn_ix, "filenameshort"] == filename+".fits")[0]]
-            print(f'wwww {filename} {filename_ix_fs}')
+            print(f'{filename} {filename_ix_fs}')
             if len(filename_ix_fs)>1:
                 raise RuntimeError(f"filename_ix_fs is returning multiple matches for filenameshort in fits_summary: {filename_ix_fs}")
 
@@ -175,22 +195,52 @@ def combine_fs_and_bg(supernova_names, fits_summary, brightest_galaxy):
                 fits_summary.at[filename_ix_fs[0], column_name] = brightest_galaxy.at[index, column_name]
     return fits_summary
 
-def get_region_command(index, fits_summary, ra_galaxy, dec_galaxy):
+
+
+def combine_fs_and_up(supernova_names, fits_summary, unmatched_pantheon):
+    for sn_name in supernova_names:
+        print("SN name: ",sn_name)
+
+        sn_ix_up = unmatched_pantheon[unmatched_pantheon["SNID"] == sn_name].index.values
+        print(f'TTTTTT',sn_ix_up)
+        if len(sn_ix_up)==0:
+            raise RuntimeError(f'Could not find SN {sn_name}')
+        elif len(sn_ix_up)>1:
+            raise RuntimeError(f'more than one entry for SN {sn_name}')
+        print('TEST1',unmatched_pantheon.loc[sn_ix_up[0],'RA'],unmatched_pantheon.loc[sn_ix_up[0],'Dec'])
+
+        sn_ix_fs = fits_summary[fits_summary["SNID"] == sn_name].index.values
+        if len(sn_ix_fs)==0:
+            raise RuntimeError(f'Could not find entries in fits_summary for SN {sn_name}')
+
+        for ix in sn_ix_fs:
+            fits_summary.loc[ix,'RA_SN']=unmatched_pantheon.loc[sn_ix_up[0],'RA']
+            fits_summary.loc[ix,'Dec_SN']=unmatched_pantheon.loc[sn_ix_up[0],'Dec']
+            
+
+    return fits_summary
+
+
+
+
+
+def get_region_commands(index, fits_summary):
 
     file_key = fits_summary.at[index, "File_key"]
     if isinstance(file_key, float) and np.isnan(file_key):
         return None, None, None
 
-    if ra_galaxy is None or dec_galaxy is None:
-        ra_galaxy = fits_summary.at[index, "RA_Galaxy"]
-        dec_galaxy = fits_summary.at[index, "Dec_Galaxy"]
+    ra_galaxy = fits_summary.at[index, "RA_Galaxy"]
+    dec_galaxy = fits_summary.at[index, "Dec_Galaxy"]
    
 
-    theta_deg, a_arcsec, b_arcsec = fits_summary.loc[index,['theta_deg','a_arcsec','b_arcsec']]
+    PA_deg, a_arcsec, b_arcsec = fits_summary.loc[index,['PA_deg','a_arcsec','b_arcsec']]
 
-    cmd = make_region_command_for_position(ra_galaxy, dec_galaxy, theta_deg, a_arcsec, b_arcsec, name=sn_name)
+    cmds = []
+    cmds.append(make_ellipse_command_for_position(ra_galaxy, dec_galaxy, PA_deg, a_arcsec, b_arcsec, name=sn_name))
+    cmds.append(make_circle_command_for_position(fits_summary.at[index, "RA_SN"], fits_summary.at[index, "Dec_SN"], name=sn_name))
 
-    return cmd, ra_galaxy, dec_galaxy
+    return cmds, ra_galaxy, dec_galaxy
 
 if __name__ == "__main__":
     args = define_args()
@@ -199,8 +249,6 @@ if __name__ == "__main__":
     print (f'Loading fits summary: {args.fits_summary_path}')
     fits_summary = pd.read_table(args.fits_summary_path,sep=',') #original
 
-    print('Test0', args.supernova_names)
-    #print('Test0000', args.supernova_names[0], args.supernova_names[1])
 
     if args.supernova_names[0].lower() == 'all':
         print('TEST1', fits_summary['SNID'])
@@ -211,12 +259,15 @@ if __name__ == "__main__":
 
     print(f'supernova_names: {supernova_names}')
 
-    
-
-
     # reading table at args.brightest_galaxy_path and assigning it to brightest_galaxy variable
     print (f'Loading brightest galaxy: {args.brightest_galaxy_path}')
     brightest_galaxy = pd.read_table(args.brightest_galaxy_path,sep=',') #original
+
+    # reading table at args.brightest_galaxy_path and assigning it to brightest_galaxy variable
+    print (f'TEST TEST TEST Loading unmatched_pantheon: {args.unmatched_pantheon_path}')
+    unmatched_pantheon = pd.read_table(args.unmatched_pantheon_path,sep=',') #original
+    print (f"AAAAAHHHHH {unmatched_pantheon}")
+    fits_summary = combine_fs_and_up(supernova_names, fits_summary, unmatched_pantheon)
 
     # receiving end: the "filenameshort" column in the fits_summary table
     fits_summary['filenameshort'] = fits_summary['filename'].str.replace(r'.*/', '', regex=True)
@@ -230,14 +281,9 @@ if __name__ == "__main__":
     if fits_summary_combined_filename == args.fits_summary_path:
         raise RuntimeError(f'EERRRRRRROORRRR: {fits_summary_combined_filename}')
 
-    print('TEST1')
-    print(fits_summary)
     get_region_info2(fits_summary)
-    print('TEST2')
-    print(fits_summary)
     print(f'Saving combined file into {fits_summary_combined_filename}')
     fits_summary.to_csv(fits_summary_combined_filename)
-
 
     pd.set_option('display.max_colwidth', None)
     
@@ -254,26 +300,56 @@ if __name__ == "__main__":
 
         sn_ix = np.where(fits_summary["SNID"] == sn_name)[0]
 
-        ra_galaxy = None # fits_summary['RA_GALAXY][sn_ix]
-        dec_galaxy = None ## fits_summary['Dec_GALAXY][sn_ix]
+        #ra_galaxy = None # fits_summary['RA_GALAXY][sn_ix]
+        #dec_galaxy = None ## fits_summary['Dec_GALAXY][sn_ix]
 
         all_output = ['global color=blue dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1']
         all_output.append('fk5')
         
-        ds9cmd = 'ds9 -zscale '
+        #sn_rootdir = f'{args.datadir}/source_extractor/{sn_name}'
+        sn_rootdir = f'{args.datadir}/pantheon_data_folder/{sn_name}/mastDownload/HST'
+
+        ds9cmd = f'\ncd {sn_rootdir} \n ds9 -zscale '
+
+        #if sn_name=='1997bq':
+
+            #print(fits_summary.loc[sn_ix,['File_key','Position_Angle','Major_Axis','Minor_Axis','a_pixels','b_pixels','theta_deg','PA_deg','a_arcsec','b_arcsec']])
+            #sys.exit(0)
 
         for index in sn_ix:
-            cmd, ra_galaxy, dec_galaxy = get_region_command(index, fits_summary, ra_galaxy, dec_galaxy)
-            if cmd is None:
+
+            if fits_summary.loc[index,"Telescope"]=='JWST':
+                print(f'WARNING! skipping since telescope={fits_summary.loc[index,"Telescope"]}')
+                continue
+
+            if fits_summary.loc[index,"Filter"]=='detection':
+                print(f'WARNING! skipping since filter={fits_summary.loc[index,"Filter"]}')
+                continue
+                
+
+            #print(f'FUCK!',fits_summary.columns)
+            #print(f'Telescope: {fits_summary.loc[index,"Telescope"]}, Filter:{fits_summary.loc[index,"Filter"]}')
+            #sys.exit(0)
+
+            cmds, ra_galaxy, dec_galaxy = get_region_commands(index, fits_summary)
+            if (cmds is None) or (cmds is []):
                 continue
 
             # make region file
             output = ['global color=blue dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1']
             output.append('fk5')
-            output.append(cmd)
-            all_output.append(cmd)
+            for cmd in cmds:
+                output.append(cmd)
+                all_output.append(cmd)
 
-            fitsfilename = f'{args.datadir}/source_extractor/{sn_name}/{fits_summary.at[index, "File_key"]}/{fits_summary.at[index, "File_key"]}.fits'
+            #fitsfilename = f'{sn_rootdir}/{fits_summary.at[index, "File_key"]}/{fits_summary.at[index, "File_key"]}.fits'
+
+            (path1,fname1) = os.path.split(fits_summary.at[index, "filename"])
+            (path2,subdir1) = os.path.split(path1)
+            fitsfilename = f'{sn_rootdir}/{subdir1}/{fname1}'
+
+            #print('bbbb',fitsfilename_old,'\n',fitsfilename)
+            #sys.exit(0)
 
             print(f'finding directory for {fitsfilename}')
             if os.path.isdir(fitsfilename):
@@ -285,14 +361,28 @@ if __name__ == "__main__":
                     print(f'ERROR: fitsfilename {fitsfilename} does not exist!!!')
 
 
+            regionfilename = re.sub('\.fits$','.reg',fitsfilename)
+            if regionfilename==fitsfilename:
+                raise RuntimeError(f'Could not replace .fits with .reg in {fitsfilename}')
+            
+            #filepath_regionfile = f"{args.out_dir}/{fits_summary.at[index, 'File_key']}_region_file.reg"
 
-            filepath_regionfile = f"{args.out_dir}/{fits_summary.at[index, 'File_key']}_region_file.reg"
+            print('fitsfilename',fitsfilename)
+            print('regionfilename',regionfilename)
 
-            ds9cmd += f' {fitsfilename} -regionfile {filepath_regionfile}'
+            if not os.path.isfile(fitsfilename):
+                print(f'WARNING!!!!! fitsfile {fitsfilename} does not exist!!!')
+                #sys.exit(0)
+                continue
+
+            relative_fitsfilename = os.path.relpath(fitsfilename, start=sn_rootdir)
+            relative_regionfilename = os.path.relpath(regionfilename, start=sn_rootdir)
+            print('fff',relative_fitsfilename)
+
+            ds9cmd += f' {relative_fitsfilename} -regionfile {relative_regionfilename}'
             print(ds9cmd)
 
-
-            save_region_file(filepath_regionfile, output)
+            save_region_file(regionfilename, output)
 
         ds9cmds_list.append(ds9cmd)
 
