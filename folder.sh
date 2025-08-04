5581c04df5f639d2c018ee27da38183f7111e34d
Bhoomika <bhoomika0khatri@gmail.com>
2025-08-04 13:47:40 -0400

    summary changes

--- folder.sh
+++ folder.sh
@@ -1,4 +1,4 @@
-export MASS_STEP_DATA_ROOTDIR='/astro/armin/bhoomika'
-export MASS_STEP_SRC_ROOTDIR='/astro/armin/bhoomika'
-export MASS_STEP_OUT_ROOTDIR=$DS9DATA_ROOTDIR/regionfiles
-export MASS_STEP_TABLES_ROOTDIR='/home/bkhatri/STScI_pantheon_Github'
+export MASS_STEP_DATA_ROOTDIR='/Users/bhoomika_khatri/Documents/data'
+export MASS_STEP_SRC_ROOTDIR='/Users/bhoomika_khatri/Documents/data'
+export MASS_STEP_OUT_ROOTDIR=$MASS_STEP_DATA_ROOTDIR/regionfiles
+export MASS_STEP_TABLES_ROOTDIR='/Users/bhoomika_khatri/Documents/STScI/STScI_pantheon'
