5c8b08a036c11754adb1f4d83448ae2e9a4f9ad3
Bhoomika <bhoomika0khatri@gmail.com>
2025-08-12 12:18:54 -0400

    probable data

--- data_collection_files/extractor_probable_info_data.py
+++ data_collection_files/extractor_probable_info_data.py
@@ -77,28 +77,28 @@ def create_prob_index(root_dir, output_csv, top_range = 0, bottom_range = 0):
     SNID = valid_filter['SNID']
     file_key = valid_filter['File_key']
     data_dict = []
+    skipped_index_dict = []
     sn1_data = pd.read_csv('/home/bkhatri/STScI_pantheon_Github/data_collection_files/data_files/unmatched_pantheon.csv')
     i = 0
+    index_on = bottom_range - 1
 
     if not os.path.exists(output_csv):
         try:
             with open(output_csv, 'x') as f:
-                f.write("This is a new file.")
+                f.write("SNID,File_key,RA_Galaxy,Dec_Galaxy,Position_Angle,Major_Axis,Minor_Axis,Kron_Radius,Mag,Mag_Error,Flux,Flux_Error,ISOAREA_IMAGE,X_IMAGE,Y_IMAGE,CXX,CXY,CYY,OFFSET,RE,d_DLR,F_HOST_4")
             print(f"File '{output_csv}' created successfully.")
         except FileExistsError:
-            # This block is theoretically not reached if os.path.exists is accurate,
-            # but provides a robust fallback for race conditions.
             print(f"File '{output_csv}' already exists")
     else:
         print(f"File '{output_csv}' already exists, ignoring creation.")
-
-
-
 
     if top_range == 0 and bottom_range == 0:
         bottom_range = len(file_key)    
     
     for index in range(top_range, bottom_range):
+
+        index_on = index_on + 1
+
         rr = sn1_data.loc[sn1_data['SNID'] == SNID[index], 'RA'].values[0]
         dd = sn1_data.loc[sn1_data['SNID'] == SNID[index], 'Dec'].values[0]
         with fits.open(imfile[index]) as hdul:
@@ -107,64 +107,71 @@ def create_prob_index(root_dir, output_csv, top_range = 0, bottom_range = 0):
             coords = SkyCoord(rr, dd, unit='deg')
             # Convert to pixel coordinates
             xx, yy = w.world_to_pixel(coords)
-        
+        print(i)
         path = glob.glob(os.path.join(root_dir, SNID[index], file_key[index], '*.cat'), recursive=True)
-        file_path = path[0]
-        df = read_sexcat(file_path)
+        try:
+            file_path = path[0]
+            df = read_sexcat(file_path)
+
+            to_rad = pi/180.0
+            Dx = xx-df['X_IMAGE']
+            Dy = yy-df['Y_IMAGE']
+            df['OFFSET']=sqrt(Dx**2+Dy**2)
+            phi = arctan(Dy/Dx)-to_rad*df['THETA_IMAGE'] ##in rad
+            xp = df['A_IMAGE']*cos(phi)*cos(to_rad*df['THETA_IMAGE'])-df['B_IMAGE']*sin(phi)*sin(to_rad*df['THETA_IMAGE'])
+            yp = df['A_IMAGE']*cos(phi)*sin(to_rad*df['THETA_IMAGE'])-df['B_IMAGE']*sin(phi)*cos(to_rad*df['THETA_IMAGE'])
+            df['RE']= df['KRON_RADIUS']*sqrt(xp**2.+yp**2.)
+            df['d_DLR']=df['OFFSET']/df['RE']
+
+            df=df.sort_values(by='OFFSET', ignore_index=True)
+
+            for nn in list([1,2,4]):
+                f_hosts=[]
+                for item in list(df['d_DLR']):
+                    tmp = 1.0-round(prob_of_gxy(item, n=nn), 5)
+                    if tmp < 0: pdb.set_trace()
+                    f_hosts.append(tmp)
+                df['F_HOST_%d'%nn]=f_hosts
+            ## pdb.set_trace()
+            df=df.sort_values(by='F_HOST_4', ignore_index=True, ascending=False)
+            probable = 0
+            info = {
+                    "SNID": SNID[index],
+                    "File_key": file_key[index], 
+                    "RA_Galaxy": df['ALPHA_J2000'][probable],
+                    "Dec_Galaxy": df['DELTA_J2000'][probable],
+                    "Position_Angle": df['THETA_IMAGE'][probable],
+                    "Major_Axis": df['A_IMAGE'][probable],
+                    "Minor_Axis": df['B_IMAGE'][probable],
+                    "Kron_Radius": df['KRON_RADIUS'][probable],
+                    "Mag": df['MAG_AUTO'][probable],
+                    "Mag_Error": df['MAGERR_AUTO'][probable],
+                    "Flux": df['FLUX_AUTO'][probable],
+                    "Flux_Error": df['FLUXERR_AUTO'][probable],
+                    "ISOAREA_IMAGE": df['ISOAREA_IMAGE'][probable],
+                    "X_IMAGE": df['X_IMAGE'][probable],
+                    "Y_IMAGE": df['Y_IMAGE'][probable],
+                    "CXX": df['CXX_IMAGE'][probable],
+                    "CXY": df['CXY_IMAGE'][probable],
+                    "CYY":df['CYY_IMAGE'][probable],
+                    "OFFSET": df['OFFSET'][probable],
+                    "RE": df['RE'][probable],
+                    "d_DLR": df['d_DLR'][probable],
+                    "F_HOST_4": df['F_HOST_4'][probable]
+                    }
+            i = i+1
+            data_dict.append(info)
+        except KeyboardInterrupt:
+            exit
+        except: 
+            print("NEED TO SKIP THIS ONE")
+            skipped_index_dict.append(skipped_index_dict)
 
 
-        to_rad = pi/180.0
-        Dx = xx-df['X_IMAGE']
-        Dy = yy-df['Y_IMAGE']
-        df['OFFSET']=sqrt(Dx**2+Dy**2)
-        phi = arctan(Dy/Dx)-to_rad*df['THETA_IMAGE'] ##in rad
-        xp = df['A_IMAGE']*cos(phi)*cos(to_rad*df['THETA_IMAGE'])-df['B_IMAGE']*sin(phi)*sin(to_rad*df['THETA_IMAGE'])
-        yp = df['A_IMAGE']*cos(phi)*sin(to_rad*df['THETA_IMAGE'])-df['B_IMAGE']*sin(phi)*cos(to_rad*df['THETA_IMAGE'])
-        df['RE']= df['KRON_RADIUS']*sqrt(xp**2.+yp**2.)
-        df['d_DLR']=df['OFFSET']/df['RE']
-
-        df=df.sort_values(by='OFFSET', ignore_index=True)
-
-        for nn in list([1,2,4]):
-            f_hosts=[]
-            for item in list(df['d_DLR']):
-                tmp = 1.0-round(prob_of_gxy(item, n=nn), 5)
-                if tmp < 0: pdb.set_trace()
-                f_hosts.append(tmp)
-            df['F_HOST_%d'%nn]=f_hosts
-        ## pdb.set_trace()
-        df=df.sort_values(by='F_HOST_4', ignore_index=True, ascending=False)
-        probable = 0
-        info = {
-                "SNID": SNID[index],
-                "File_key": file_key[index], 
-                "RA_Galaxy": df['ALPHA_J2000'][probable],
-                "Dec_Galaxy": df['DELTA_J2000'][probable],
-                "Position_Angle": df['THETA_IMAGE'][probable],
-                "Major_Axis": df['A_IMAGE'][probable],
-                "Minor_Axis": df['B_IMAGE'][probable],
-                "Kron_Radius": df['KRON_RADIUS'][probable],
-                "Mag": df['MAG_AUTO'][probable],
-                "Mag_Error": df['MAGERR_AUTO'][probable],
-                "Flux": df['FLUX_AUTO'][probable],
-                "Flux_Error": df['FLUXERR_AUTO'][probable],
-                "ISOAREA_IMAGE": df['ISOAREA_IMAGE'][probable],
-                "X_IMAGE": df['X_IMAGE'][probable],
-                "Y_IMAGE": df['Y_IMAGE'][probable],
-                "CXX": df['CXX_IMAGE'][probable],
-                "CXY": df['CXY_IMAGE'][probable],
-                "CYY":df['CYY_IMAGE'][probable],
-                "OFFSET": df['OFFSET'][probable],
-                "RE": df['RE'][probable],
-                "d_DLR": df['d_DLR'][probable],
-                "F_HOST_4": df['F_HOST_4'][probable]
-                }
-        i = i+1
-
-        if i%10 == 0:
-            df = pd.DataFrame(data_dict)
-            df.to_csv('most_probable_gal.csv', mode = 'a', index=False)
-        data_dict.append(info)
+    dictionary = pd.DataFrame(data_dict)
+    dictionary.to_csv(output_csv, mode = 'a', index=False, header = False)
+    skipped = pd.DataFrame(skipped_index_dict)
+    skipped.to_csv("skipped_index.csv", index = False)
 
     
 
@@ -177,4 +184,4 @@ if __name__=='__main__':
     #root_dir = os.path.join(home_dir, 'source_extractor')
     #print(root_dir)
     relative_key = 0
-    create_prob_index(source_extractor_path, 'most_probable_gal.csv',tr, br)
+    create_prob_index(source_extractor_path, 'probable_galaxy_catalog.csv',tr, br)
