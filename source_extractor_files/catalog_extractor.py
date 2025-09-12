8afd799e8b41a9d415ae38e8b2dbb3ca1b75e9f3
Estefania Padilla Gonzalez <estefania8496@gmail.com>
2025-09-12 15:13:56 -0400

    updated the probable catalog

--- source_extractor_files/catalog_extractor.py
+++ source_extractor_files/catalog_extractor.py
@@ -21,20 +21,25 @@ def source_extractor(source_extractor_path = ""):
         subprocess.call("sex -d -> default.sex", shell=True)
         print("HEY MAKE THE DEFAULT SEX FILE")
 
-def catalog_creator(SNID, filename, base, filter, home_path, sex_file_name = 'default', source_path = ""):
+def catalog_creator(SNID, filename, base, filter, home_path, sex_file_name = 'default', source_path = "", force=False):
     #Check if the filter is of type detection (full stack image). Don't go through these
     if filter == 'detection':
         return;
-    #Check if source extractor was already done. Don't do these
-    done = check_file_exist(home_path, source_path, SNID, base)
-    if done == True:
-        return;
+    #Check if source extractor was already done. Don't do these (unless force=True)
+    if not force:
+        done = check_file_exist(home_path, source_path, SNID, base)
+        if done == True:
+            return;
 
     dest_path = filename
-    exec_string = 'sex ' + dest_path + ' -c ' + sex_file_name + '.sex'
+    # if the instrument is JWST, then we need to specify extension 1
+    if base.startswith('jw'):
+        exec_string = 'sex ' + dest_path + '[1] -c ' + sex_file_name + '.sex'  # [1] specifies extension 1
+    else:
+        exec_string = 'sex ' + dest_path + ' -c ' + sex_file_name + '.sex'
 
     
-    cat_file_name = f"{sex_file_name}.cat"
+    cat_file_name = "test.cat"  # This matches the CATALOG_NAME in default.sex
     new_cat_file_name = f"{base}.cat"
 
     fits_file_name = "check.fits"
@@ -70,6 +75,12 @@ def move_files_group(SNid, base, cat_file_name, fits_file_name, home_path, sourc
     try:
         destination_path = f"{home_path}/{source_path}/{SNid}/{base}"
         os.makedirs(destination_path, exist_ok=True)
+        
+        # Remove existing file if it exists (to allow overwriting)
+        dest_file = os.path.join(destination_path, cat_file_name)
+        if os.path.exists(dest_file):
+            os.remove(dest_file)
+            
         shutil.move(cat_file_name, destination_path)
         #shutil.move(fits_file_name, destination_path)
     except Exception as e:
@@ -79,16 +90,19 @@ def move_files_group(SNid, base, cat_file_name, fits_file_name, home_path, sourc
 
 
 def main():
-    full_dataframe = pd.read_csv('/home/bkhatri/STScI_pantheon/data_collection_files/data_files/fits_summary.csv')
+    full_dataframe = pd.read_csv('/home/epadill/photometry_ds9/STScI_pantheon/data_collection_files/data_files/fits_summary.csv')
+    #get the rows where telescope is JWST
+    full_dataframe = full_dataframe[full_dataframe['Telescope'] == 'JWST']
+    print(full_dataframe['filename'].str.split('/').str[-1])    #for testing
 
     #activates the conda enviroment
-    conda_act = "conda activate .conda"
-    subprocess.call("conda init", shell = True)
-    subprocess.call(conda_act, shell=True)
+    # Note: conda activation in subprocess doesn't work well
+    # Make sure to run this script in the correct environment
+    pass
 
     df = full_dataframe[start:end]
     #Makes sure that each of the source extractor files are avaliable and found
-    source_extractor(source_extractor_path= "/home/bkhatri/STScI_pantheon/")
+    source_extractor(source_extractor_path= "/home/epadill/photometry_ds9/STScI_pantheon/")
     for row in df.itertuples():
         catalog_creator(SNID = row.SNID, 
                         filename = row.filename, 
@@ -96,7 +110,8 @@ def main():
                         filter = row.Filter, 
                         home_path = "/astro/armin/bhoomika",
                         sex_file_name = sex_name, 
-                        source_path= "source_extractor")
+                        source_path= "source_extractor",
+                        force=True)
 
 if __name__=="__main__":
     start = int(sys.argv[1])
